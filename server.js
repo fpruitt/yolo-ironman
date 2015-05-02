dd914e87dc54bac61066c4f2e270e11a449e150e
Andrew McPherson <Andrew@Blackbox.local>
2015-05-02 15:44:54 -0400

    Added some boilerplate for html and css.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+var express = require("express")
+var server = express()
+
+server.use(express.static(__dirname))
+
+server.listen(1337);
 console.log('Server running at http://127.0.0.1:1337/');
