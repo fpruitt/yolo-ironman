eba513e20828a21a6ee8f82daf493cb2860afef4
Forrest Pruitt <fpruitt0922@gmail.com>
2015-05-02 18:37:24 -0400

    adding moving red box, like a boss

--- source/scripts/components/Game.js
+++ source/scripts/components/Game.js
@@ -1,13 +1,20 @@
 var Loop = require("<scripts>/utilities/Loop")
 var Input = require("<scripts>/utilities/Input")
 
-var speed = 1
+
 
 var jack = {
     position: {
         x: 5,
         y: 10
-    }
+    },
+    velocity: {
+        x: 0,
+        y: 0,
+    },
+    max_velocity: .25,
+    acceleration: 5,
+    deacceleration: 2
 }
 
 var GameFrame = require("<scripts>/components/GameFrame")
@@ -23,23 +30,58 @@ var Game = React.createClass({
     },
     componentDidMount: function() {
         Loop(function(delta) {
+            // Input Processing
         	if(Input.isHammered(87)
         	|| Input.isHammered(38)) {
-        		jack.position.y -= speed
+        		jack.velocity.y -= jack.acceleration *delta
         	}
         	if(Input.isHammered(83)
         	|| Input.isHammered(40)) {
-        		jack.position.y += speed
+        		jack.velocity.y += jack.acceleration * delta
         	}
         	if(Input.isHammered(65)
         	|| Input.isHammered(37)) {
-        		jack.position.x -= speed
+        		jack.velocity.x -=  jack.acceleration *delta
         	}
         	if(Input.isHammered(68)
         	|| Input.isHammered(39)) {
-        		jack.position.x += speed
+        		jack.velocity.x +=  jack.acceleration * delta
         	}
-
+            if(jack.velocity.x > jack.max_velocity)
+                jack.velocity.x = jack.max_velocity
+            if(jack.velocity.x < -jack.max_velocity)
+                jack.velocity.x = -jack.max_velocity
+                
+            if(jack.velocity.y > jack.max_velocity)
+                jack.velocity.y = jack.max_velocity
+            if(jack.velocity.y < -jack.max_velocity)
+                jack.velocity.y = -jack.max_velocity
+            // Translation
+            jack.position.x += jack.velocity.x
+            jack.position.y += jack.velocity.y
+            // Deacceleration 
+            if(jack.velocity.x > 0)
+            {
+                jack.velocity.x -= jack.deacceleration * delta
+                if(jack.velocity.x < 0)
+                    jack.velocity.x = 0
+            }
+            else if (jack.velocity.x < 0) {
+                jack.velocity.x += jack.deacceleration * delta
+                if(jack.velocity.x > 0 )
+                    jack.velocity.x = 0
+            }
+	        if(jack.velocity.y > 0)
+            {
+                jack.velocity.y -= jack.deacceleration * delta
+                if(jack.velocity.y < 0)
+                    jack.velocity.y = 0
+            }
+            else if (jack.velocity.y < 0) {
+                jack.velocity.y += jack.deacceleration * delta
+                if(jack.velocity.y > 0 )
+                    jack.velocity.y = 0
+            }
             this.forceUpdate()
         }.bind(this))
     }
