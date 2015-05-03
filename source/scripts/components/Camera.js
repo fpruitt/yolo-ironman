b0b0626ccdf3c381297db433743df6f776e143fd
Andrew McPherson <andrewmcp333@gmail.com>
2015-05-03 10:09:38 -0400

    moved camera to own file

---
+++ source/scripts/components/Camera.js
@@ -0,0 +1,19 @@
+var Camera = React.createClass({
+    render: function() {
+        return (
+            <div style={this.renderStyles()}>
+                {this.props.children}
+            </div>
+
+        )
+    },
+    renderStyles: function(){
+        return {
+            position: 'absolute',
+            top: this.props.target.position.y * -1 + HEIGHT/2 + 'em',
+            left: this.props.target.position.x * -1 + WIDTH/2 + 'em'
+        }
+    }
+})
+
+module.exports = Camera
--- source/scripts/components/Game.js
+++ source/scripts/components/Game.js
@@ -12,7 +12,8 @@ var world = {
         return this.tiles[Math.floor(x) + "x" + Math.floor(y)]
     }
 }
-
+world.width = tiledmap.width
+world.height = tiledmap.height
 var tiles = tiledmap.layers[0].data
 for(var x = 0; x < tiledmap.width; x++) {
     for(var y = 0; y < tiledmap.height; y++) {
@@ -22,12 +23,11 @@ for(var x = 0; x < tiledmap.width; x++) {
                 y: y
             },
             image: tiles[y * tiledmap.width + x],
-            passable: tiles[y * tiledmap.width + x] != 149 && tiles[y * tiledmap.width + x] != 195
+            passable: tiles[y * tiledmap.width + x] != 149+1
+                   && tiles[y * tiledmap.width + x] != 194+1
         }
     }
 }
-world.width = tiledmap.width
-world.height = tiledmap.height
 
 var jack = {
     position: {
@@ -44,24 +44,7 @@ var jack = {
 }
 
 var GameFrame = require("<scripts>/components/GameFrame")
-
-var Camera = React.createClass({
-    render: function() {
-        return (
-            <div style={this.renderStyles()}>
-                {this.props.children}
-            </div>
-
-        )
-    },
-    renderStyles: function(){
-        return {
-            position: 'absolute',
-            top: this.props.target.position.y * -1 + HEIGHT/2 + 'em',
-            left: this.props.target.position.x * -1 + WIDTH/2 + 'em'
-        }
-    }
-})
+var Camera = require("<scripts>/components/Camera")
 
 var Game = React.createClass({
     render: function() {
@@ -79,7 +62,7 @@ var Game = React.createClass({
             // Input Processing
         	if(Input.isHammered(87)
         	|| Input.isHammered(38)) {
-        		jack.velocity.y -= jack.acceleration *delta
+        		jack.velocity.y -= jack.acceleration * delta
         	}
         	if(Input.isHammered(83)
         	|| Input.isHammered(40)) {
@@ -87,7 +70,7 @@ var Game = React.createClass({
         	}
         	if(Input.isHammered(65)
         	|| Input.isHammered(37)) {
-        		jack.velocity.x -=  jack.acceleration *delta
+        		jack.velocity.x -=  jack.acceleration * delta
         	}
         	if(Input.isHammered(68)
         	|| Input.isHammered(39)) {
@@ -107,26 +90,23 @@ var Game = React.createClass({
                 jack.position.x += jack.velocity.x;
             else
                 jack.velocity.x = 0;
-            
+
             if(world.getTile(jack.position.x, jack.position.y+jack.velocity.y).passable)
                 jack.position.y += jack.velocity.y;
             else
                 jack.velocity.y = 0;
-            
             // Deacceleration
-            if(jack.velocity.x > 0)
-            {
+            if(jack.velocity.x > 0) {
                 jack.velocity.x -= jack.deacceleration * delta
                 if(jack.velocity.x < 0)
-                    jack.velocity.x = 0;
+                    jack.velocity.x = 0
             }
             else if (jack.velocity.x < 0) {
                 jack.velocity.x += jack.deacceleration * delta
                 if(jack.velocity.x > 0 )
-                    jack.velocity.x = 0;
+                    jack.velocity.x = 0
             }
-	        if(jack.velocity.y > 0)
-            {
+	        if(jack.velocity.y > 0) {
                 jack.velocity.y -= jack.deacceleration * delta
                 if(jack.velocity.y < 0)
                     jack.velocity.y = 0
